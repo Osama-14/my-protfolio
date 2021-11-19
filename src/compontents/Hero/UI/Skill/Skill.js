8e2ef205d7da5e17a9310779d20813ad67f5d2ad
Osama-14 <mohdosama42000@gmail.com>
2021-11-19 23:32:15 +0500

    changes

--- src/compontents/Hero/UI/Skill/Skill.js
+++ src/compontents/Hero/UI/Skill/Skill.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "react-circular-progressbar/dist/styles.css";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-
+import './style.css'
 
 const Skill = (props) => {
   return (
-    <div>
-      <CircularProgressbar
+    <div className="flexRow align-center">
+        <div className="skillContainer"> 
+        <CircularProgressbar
         value={props.value}
         text={`${props.value}%`}
         styles={buildStyles({
@@ -14,9 +15,13 @@ const Skill = (props) => {
           pathColor: props.pathColor,
         })}
       />
-        <div>
-            <p> {props.skillName}</p>
-            <p> {props.desc}</p>
+
+        </div>
+
+     
+        <div className="mlr-10">
+            <p className="font-16 bold-500 textColor mtb-10"> {props.skillName}</p>
+            <p className="font-16 bold-500 grey mtb-10"> {props.desc}</p>
 
         </div>
 
--- src/compontents/Specializing/Index.js
+++ src/compontents/Specializing/Index.js
@@ -3,9 +3,17 @@ import MediumHeading from '../Hero/UI/MediumHeading';
 import SmallHeading from '../Hero/UI/SmallHeading';
 import Card from '../../compontents/Hero/UI/Card/index'
 import Skill from '../Hero/UI/Skill/Skill'
+import { Colors } from '../../Style';
 
 
 const Specializing = (props) => {
+    const skills = [ {
+        skillName: 'HTML5/CSS3',
+        desc:'Hands on experience in Html/Css',
+        textColor: Colors.primaryColor,
+        pathColor:'#f2a154'
+    } ]
+
     return (
         <div className="container">
 
@@ -14,14 +22,15 @@ const Specializing = (props) => {
            <MediumHeading  text="Specializing"/>
 
            <div>
+               
 
                <Skill
 
                 skillName="HTML/CSS"
                 desc={`Hands on experience in Html5/CSS3`}
                 value="70"
-                textColor="red"
-                pathColor="green"
+                textColor={Colors.primaryColor}
+                pathColor="#FF7800"
 
                
                />
